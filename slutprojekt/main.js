2bbe0d06fbbae1a8fa51df1689e553a0d8253c42
AmarAlRubaie <Amaralrubaie080301@gmail.com>
2026-03-31 09:29:59 +0200

    dsf

--- slutprojekt/main.js
+++ slutprojekt/main.js
@@ -1,7 +1,13 @@
 
 function toggleMenu(){
     document.querySelector("nav").classList.toggle("showMenu");
+    document.querySelector("main").classList.toggle("darkerMain");
+    document.querySelector("div.announcement-bar").classList.toggle("darkerMain");
+    document.querySelector("div.top").classList.toggle("darkerMain");
 }
 function hideMenu(){
     document.querySelector("nav").classList.remove("showMenu");
+    document.querySelector("main").classList.remove("darkerMain");
+    document.querySelector("div.announcement-bar").classList.remove("darkerMain");
+    document.querySelector("div.top").classList.remove("darkerMain");
 }
